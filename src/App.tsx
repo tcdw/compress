933c3aeb0b86d7e08918b9e5d26e528fa15cda4f
Xi Zhang <tcdw2011@gmail.com>
2026-01-06 11:01:24 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "@/components/ui/button";
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button>Hello!</Button>
+    </>
+  );
+};
+
+export default App;
